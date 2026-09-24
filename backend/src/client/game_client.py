3ca98ef0e05b6ecd388ce6623bff76382f8cf411
cecilelocatelli21 <cecile.locatelli@icloud.com>
2026-09-24 09:09:02 +0000

    Question 4.1, 2 , 3

---
+++ backend/src/client/game_client.py
@@ -0,0 +1,33 @@
+import json
+
+import requests
+
+from business_object.game import Game
+from business_object.player import Player
+
+
+class GameClient():
+    def get_games() -> list[Game]:
+        r = requests.get(url="http://127.0.0.1:5555")
+        if r.status_code != 200:
+            raise Exception(f"Cannot reach (HTTP {r.status_code}): {r.text}")
+        else:
+            raw_json = r.json()
+            print(json.dumps(raw_json, indent=2))  # Pretty print
+        games = []
+
+        for elt in raw_json:
+            # Create an object
+            g = Game(
+                player1=elt["players_list"][0],
+                player2=elt["players_list"][1],
+                game_mode=elt["mode_type"],
+                winner=elt["winner_name"],
+                description=elt[""],
+                timestamp=elt["datetime"],
+                id_game=elt["id_game"]
+            )
+
+            # If it succeed, add to the list
+            if g:
+                games.append(g)
